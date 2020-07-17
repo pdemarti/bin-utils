1c2b5a1ef3850c4cc24e478b2baa66882bf55ab3
Pierre Demartines <pierre@demartines.com>
2020-07-16 20:34:58 -0400

    some more exploration

--- bin/perl/add.pl
+++ bin/perl/add.pl
@@ -4,30 +4,40 @@
 # P. Demartines <pierred@lexicus.mot.com> 26-Sep-1997
 # PD 15-Jan-01 modified for -c option (cumul)
 
-require "getopts.pl";
+use Getopt::Std;
 
-sub err_usage
-  {
+our $opt_c;
+our $opt_k;
+our $opt_v;
+
+sub err_usage {
   print STDERR "
 Usage is:
-$0 [-c] [file]
+$0 [-c] [-k k] [-v] [file]
     -c: cumulate. This prints out the cumul of numbers as we find them when
         going through the file, rather than just showing the final sum.
+    -k k: limit to only the first k numbers found on each line.
+    -v: echo each line of the input file (implies -c)
 ";
   exit @_;
-  }
-
-if (!&Getopts('c')) {do err_usage(200);}
-
-while (<>) {
-    @sumFoundOnThisLine = () if ($opt_c);
-    while (s/\b[-]?(\d+\.?\d*|\.\d+)([eE][-]?\d+)?\b//) {
-	$sum += $&;
-	push(@sumFoundOnThisLine, $sum) if ($opt_c);
-    }
-    if ($opt_c) {
-	print join(" ", @sumFoundOnThisLine) . "\n";
-    }
 }
 
-print "$sum\n" unless ($opt_c);
+err_usage(200) unless getopts('hck:v');
+err_usage(0) if $opt_h;
+
+while (<>) {
+    $line = $opt_v ? $_ : "\n";
+    # remove comments
+    s/\#.*//;
+    @a = split(/([-]?(?:\d+\.?\d*|\.\d+)(?:[eE][-]?\d+)?)/);
+    # take only the odd elements in the array (i.e.: the numbers)
+    @a = @a[map 2*$_+1, 0..$#a/2];
+
+    # limit to only first number (if any)
+    splice @a, $opt_k if $opt_k;
+    $sum += $_ for @a;
+
+	print "$sum\t$line" if $opt_c || $opt_v;
+}
+
+print "$sum\n" unless $opt_c;
